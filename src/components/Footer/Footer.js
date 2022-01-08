e89fca47d2f479e224d3200b3978dbbb14ba834d
Islombek <sultonovislom9717@gmail.com>
2022-01-08 23:22:40 +0500

    finish all components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+998932724484">+998932724484</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:sultonovislom9717@gmail.com">
+            sultonovislom9717@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Islombek9717">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/islombek-sultonov-980311175/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.instagram.com/islombek9717/">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,56 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Youtube Clone",
+    description:
+      "In this project, I use useState, useEffect, Context, Routing and some react skills",
+    image:
+      "https://reactjsexample.com/content/images/2021/09/youtube-clone.jpg",
+    tags: ["React", "Styled-Components", "Hooks", "Context"],
+    source: "https://github.com/Islombek9717/youtube-clone",
+    visit: "https://yotube-clone-9717.netlify.app/",
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Furniking",
+    description:
+      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use useState and useEffect for data.And also you will learn work with styled components. On top of that,  you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    image: "https://tgraph.io/file/069f992319920a6b30c5e.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/Islombek9717/furniking",
+    visit: "https://furniking-net.netlify.app/",
     id: 1,
   },
   {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Online Cinema Ticket Shop",
+    description:
+      "In this project I use my all HTML and CSS knowladge. I use hover, active, focus effects and also sass mixsins",
+    image:
+      "https://design4users.com/wp-content/uploads/2017/09/cinema-app-desktop.png",
+    tags: ["HTML", "CSS", "SASS"],
+    source: "https://github.com/Islombek9717/cinema_ticket_form_demo",
+    visit: "https://online-cinema.netlify.app/",
     id: 2,
   },
   {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "YouTube Clone",
+    description:
+      "Using React, Styled-components, Hooks & Context you'll learn how to build a Youtube Clone Application - from start to finish. The App is called Youtube and it is a simple social media app that allows to watch and search videos.",
+    image:
+      "http://ui4free.com/storage/public/images/figma-youtube-clone-free-template_1631610368.jpg",
+    tags: ["React", "Styled-components", "Hooks"],
+    source: "https://github.com/Islombek9717/youtubeCLone",
+    visit: "https://youtube-net-clone.netlify.app/",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: "Started my journey" },
+  {
+    year: 2018,
+    text: "Started bachelor as a computer science student at Sejong university",
+  },
+  { year: 2019, text: "Started work as a web developer" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
 ];
